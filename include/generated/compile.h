0318e8057574fdf7b791acad6e237ad215f775fc
brymaster5000 <brymaster5000@gmail.com>
2013-05-30 22:17:53 -0400

    added force fast charge

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 12 */
+/* This file is auto generated, version 13 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#12 PREEMPT Fri Jun 7 23:39:58 EDT 2013"
+#define UTS_VERSION "#13 PREEMPT Sat Jun 8 00:13:22 EDT 2013"
 #define LINUX_COMPILE_BY "brymaster5000"
 #define LINUX_COMPILE_HOST "bryslaptop"
 #define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
