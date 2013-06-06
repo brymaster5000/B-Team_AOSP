994b8eeb42494565f0f9430b22b2843af50f59d0
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-06-06 09:56:57 -0400

    source upload , cam still a no-go

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 8 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#8 PREEMPT Thu Jun 6 09:21:37 EDT 2013"
+#define LINUX_COMPILE_BY "bsmitty2"
+#define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
+#define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
