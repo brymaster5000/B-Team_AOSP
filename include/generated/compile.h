bf62f16840464ac63b7d9a21134d15bb81d62afb
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-06-15 13:42:17 -0400

    wasted alot of time lol

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-/* This file is auto generated, version 8 */
+/* This file is auto generated, version 1 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#8 PREEMPT Sun Jun 9 12:24:32 EDT 2013"
+#define UTS_VERSION "#1 PREEMPT Sat Jun 15 13:37:31 EDT 2013"
 #define LINUX_COMPILE_BY "bsmitty2"
 #define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
-=======
-/* This file is auto generated, version 2 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 PREEMPT Sun Jun 9 13:00:27 EDT 2013"
-#define LINUX_COMPILE_BY "brymaster5000"
-#define LINUX_COMPILE_HOST "bryslaptop"
->>>>>>> 3044d32290404dfc93a6b2ddada4ec1c94848114
-#define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
+#define LINUX_COMPILER "gcc version 4.8.1 20130506 (prerelease) (crosstool-NG hg+default-4c63ce24fccd - Linaro GCC 2013.05) "
