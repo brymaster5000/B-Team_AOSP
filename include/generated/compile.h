060d3e96956d8cbcb6a6243ba6139f49eb114f46
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-06-20 00:15:05 -0400

    good build

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Wed Jun 19 23:31:18 EDT 2013"
+#define UTS_VERSION "#2 PREEMPT Thu Jun 20 00:11:47 EDT 2013"
 #define LINUX_COMPILE_BY "bsmitty2"
 #define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
 #define LINUX_COMPILER "gcc version 4.7.3 20130205 (prerelease) (crosstool-NG hg+default-67df47c6ccd0 - Linaro GCC 2013.02 - Cortex-A8) "
