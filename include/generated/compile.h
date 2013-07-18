d5758726ed92bbf0e8fdcf8a02ad23802ab18ac8
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-07-18 05:53:53 -0400

    clean up files , new build with xz compression on linaro 4.8.2

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 9 */
+/* This file is auto generated, version 11 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#9 PREEMPT Wed Jul 10 21:55:52 EDT 2013"
+#define UTS_VERSION "#11 PREEMPT Thu Jul 18 05:24:19 EDT 2013"
 #define LINUX_COMPILE_BY "bsmitty2"
 #define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
-#define LINUX_COMPILER "gcc version 4.7.3 20130205 (prerelease) (crosstool-NG hg+default-67df47c6ccd0 - Linaro GCC 2013.02 - Cortex-A8) "
+#define LINUX_COMPILER "gcc version 4.8.2 20130624 (prerelease) (crosstool-NG hg+default-81ead21f86c2 - Linaro GCC 2013.07) "
