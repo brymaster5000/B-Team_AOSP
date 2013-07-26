5b1b2faa94bff45abe4791919e74dda7306042d0
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-07-26 18:27:41 -0400

    Added ARM and NEON specific optimizations flags --faux123

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 12 */
+/* This file is auto generated, version 13 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#12 PREEMPT Fri Jul 26 06:42:14 EDT 2013"
+#define UTS_VERSION "#13 PREEMPT Fri Jul 26 18:21:07 EDT 2013"
 #define LINUX_COMPILE_BY "bsmitty2"
 #define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
 #define LINUX_COMPILER "gcc version 4.8.2 20130624 (prerelease) (crosstool-NG hg+default-81ead21f86c2 - Linaro GCC 2013.07) "
